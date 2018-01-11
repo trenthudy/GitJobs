2715e8006d938ccde82765529aca7b9008b54023
Unknown <trenthudy@gmail.com>
2018-01-11 13:44:12 -0500

    create pojo model for GitHub Job json obj

---
+++ githubjobs/src/main/java/io/hudepohl/githubjobs/obj/GitHubJob.java
@@ -0,0 +1,100 @@
+package io.hudepohl.githubjobs.obj;
+
+/**
+ * Created by trent on 1/11/18.
+ */
+
+public class GitHubJob {
+
+    public String id;
+    public String created_at;
+    public String title;
+    public String location;
+    public String type;
+    public String description;
+    public String how_to_apply;
+    public String company;
+    public String company_url;
+    public String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getHow_to_apply() {
+        return how_to_apply;
+    }
+
+    public void setHow_to_apply(String how_to_apply) {
+        this.how_to_apply = how_to_apply;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCompany_url() {
+        return company_url;
+    }
+
+    public void setCompany_url(String company_url) {
+        this.company_url = company_url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
