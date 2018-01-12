3bb9ac199fdc36c01d9e72ddae99a116fa483740
Unknown <trenthudy@gmail.com>
2018-01-12 13:21:13 -0500

    add base classes

---
+++ app/src/main/java/io/hudepohl/gitjobs/app/BaseApp.java
@@ -0,0 +1,16 @@
+package io.hudepohl.gitjobs.app;
+
+import android.app.Application;
+
+/**
+ * Created by trent on 1/12/18.
+ */
+
+public class BaseApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
---
+++ app/src/main/java/io/hudepohl/gitjobs/ui/BaseActivity.java
@@ -0,0 +1,17 @@
+package io.hudepohl.gitjobs.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by trent on 1/12/18.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
